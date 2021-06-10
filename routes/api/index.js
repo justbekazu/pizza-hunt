879651a071d6b01668b215efcf193f2cd11969f6
djkaz <justbekazu@gmail.com>
2021-06-10 18:28:03 -0500

    added the ability to delete comments

---
+++ routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
+
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
+
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
